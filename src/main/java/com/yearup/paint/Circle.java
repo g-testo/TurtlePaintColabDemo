5f79ac6d393cec8b81ca2ec95612c32911d72401
pavelnosarev <pavelnosarev@outlook.com>
2023-05-25 15:37:30 -0400

    added method to circle

--- src/main/java/com/yearup/paint/Circle.java
+++ src/main/java/com/yearup/paint/Circle.java
@@ -1,4 +1,27 @@
 package com.yearup.paint;
 
 public class Circle {
+    private int radius;
+
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle{" +
+                "radius=" + radius +
+                '}';
+    }
+
+    private Circle(int radius) {
+        this.radius = radius;
+
+
+    }
 }
