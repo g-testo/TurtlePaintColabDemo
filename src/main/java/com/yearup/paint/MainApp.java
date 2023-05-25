22f5893defbd27ca7d4090541ea03a9ed48fdcb3
pavelnosarev <pavelnosarev@outlook.com>
2023-05-25 15:14:35 -0400

    hi

--- src/main/java/com/yearup/paint/MainApp.java
+++ src/main/java/com/yearup/paint/MainApp.java
@@ -11,30 +11,30 @@ public class MainApp
         // the TurtleLogo application works
 
         // The world is your canvas
-        World world = new World(200, 200);
-        Turtle turtle = new Turtle(world,-100, -100);
-        
-        int width = 200;
-        int height = 200;
+//        World world = new World(200, 200);
+//        Turtle turtle = new Turtle(world,-100, -100);
+//
+//        int width = 200;
+//        int height = 200;
 
         // calculate the hypotenuse (diagonal)
         // a2 + b2 = c2
-        double widthSquared = Math.pow(width, 2);
-        double heightSquared = Math.pow(height, 2);
-        double hypotenuse = Math.sqrt(widthSquared + heightSquared);
-
-
-        turtle.setPenWidth(3);
-        turtle.setColor(Color.GREEN);
-
-        turtle.turnRight(45);
-        turtle.forward(hypotenuse);
-
-        turtle.penUp();
-        turtle.goTo(100, 100);
-        turtle.turnRight(90);
-
-        turtle.penDown();
-        turtle.forward(hypotenuse);
+//        double widthSquared = Math.pow(width, 2);
+//        double heightSquared = Math.pow(height, 2);
+//        double hypotenuse = Math.sqrt(widthSquared + heightSquared);
+//
+//
+//        turtle.setPenWidth(3);
+//        turtle.setColor(Color.GREEN);
+//
+//        turtle.turnRight(45);
+//        turtle.forward(hypotenuse);
+//
+//        turtle.penUp();
+//        turtle.goTo(100, 100);
+//        turtle.turnRight(90);
+//
+//        turtle.penDown();
+//        turtle.forward(hypotenuse);
     }
 }
